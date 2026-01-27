9cefc5c41956da95d0637f20e73b025661de13a9
ikkevijay-hub <>
2026-01-27 13:23:04 +0000

    Variable assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,11 @@
+# Define the grocery item details
+item_name = "Eggs"
+item_price = 3.99
+item_stock_quantity = 120
+in_stock = True
+
+# Testing
+print("Eggs : ", item_name)
+print("3.99 : $", item_price)
+print("120: ", item_stock_quantity)
+print("True: ", in_stock)
