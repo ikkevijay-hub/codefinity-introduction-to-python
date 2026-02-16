f334f67df7e09dd3f0173b9c10eef464b3887147
ikkevijay-hub <>
2026-02-16 18:26:36 +0000

    Variable Assignment

--- variables_and_types/variable_assignment/main.py
+++ variables_and_types/variable_assignment/main.py
@@ -1,11 +1,45 @@
-# Define the grocery item details
 item_name = "Eggs"
 item_price = 3.99
 item_stock_quantity = 120
-in_stock = True
+in_stock = True 
 
 # Testing
-print("Eggs : ", item_name)
-print("3.99 : $", item_price)
-print("120: ", item_stock_quantity)
-print("True: ", in_stock)
+#print("Item : ", item_name)
+#print("Price per unit: $", item_price)
+#print("quantity in stock: ", item_stock_quantity)
+#print("In stock: ", in_stock)
+
+item_name = "Banana"
+
+#print("Item : ", item_name)
+#print("Price per unit: $", item_price)
+#print("quantity in stock: ", item_stock_quantity)
+#print("In stock: ", in_stock)
+
+item_name = " Choclate "
+item_price = 5
+item_stock_quantity = 20
+in_stock =True
+
+#print("Item:", item_name )
+#print ("Price per Unit:", item_price)
+#print("Quantity in Stock: ", item_stock_quantity)
+#print("In Stock: ",in_stock)
+
+
+
+customer_name =  "Rahul"
+account_balance = 15000 
+interest_rate = 6.75
+is_account_active = True 
+transactions = [5000, -2000, 3000] 
+bank_account = {"Name": "Anita", 
+                "balance": 25000, 
+                "active": True }
+
+print("Customer Name :", customer_name)
+print("Account Balance :", account_balance)
+print("Interest Rate Assigned to Customer :", interest_rate)
+print("Is Account Active :", is_account_active)
+print("Allowed Transactions :", transactions)
+print("Bank Detail :", bank_account)
