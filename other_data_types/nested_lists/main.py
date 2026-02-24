471ebed65423d7c9a301331344be5e08aa9d4d65
ikkevijay-hub <>
2026-02-24 11:45:26 +0000

    Nested Lists

--- other_data_types/nested_lists/main.py
+++ other_data_types/nested_lists/main.py
@@ -14,17 +14,43 @@ vegetables.remove("onions")
 vegetables.sort()
 print("Updated Vegetable Inventory:", vegetables)
 
-balances = [2000, 8000, 12000, 4000, 7100]
-for balance in balances:
+print("#############################################################")
 
-    if balance < 3000:
-        print(balance, "→ Account Block Risk")
+pipeline_stages = ["build", "test", "scan"]
+failed_stages = []
+approved_environments = ["dev", "uat"]
+deployment_history = ["dev-build-001", "uat-build-002"]
 
-    elif 3000 <= balance <= 7000:
-        print(balance, "→ Maintain Balance Warning")
+new_stage = "deploy"
+environment = "prod"
+new_build = "prod-build-003"
 
-    else:
-        print(balance, "→ Healthy Account")
+          
+pipeline_stages.insert(3,new_stage)
+print(pipeline_stages)
+pipeline_stages.remove("scan")
+print(pipeline_stages)
+pipeline_stages.sort()
+print(pipeline_stages)
+if environment not in approved_environments:
+    failed_stages.append("environment")
+print(approved_environments)
+deployment_history.insert(2, new_build)
+print(deployment_history)
+pipeline_stages.reverse()
+print(pipeline_stages)
+deployment_history.remove("dev-build-001")
+print(deployment_history)
+approved_environments.copy()
+print(approved_environments.copy())
+pipeline_stages.count("deploy")
+print(pipeline_stages.count("deploy"))
+pipeline_stages.index("test")
+print(pipeline_stages.index("test"))
 
-           
+
+
+
+
+
 
