bdcde0001005e50ead5fd3598d6d25e019d3283c
ikkevijay-hub <>
2026-03-10 13:22:07 +0000

    User-defined functions

--- functions/user_defined_functions/main.py
+++ functions/user_defined_functions/main.py
@@ -5,31 +5,56 @@ items = ["Milk", "Bread", "Eggs", "Rice", "Apples", "Soap"]
 prices = [30, 25, 60, 80, 45, 20]
 
 combined_list = list(zip(items, prices))
-for item, price, in combined_list:
-    print(f"Product: {item} : {price}")
 
 total = 0
-for item, price, in combined_list:
-    total += price
-print(f"Total Bill :{total}")
 
-#if prices % 2 == 0:
- #   total += prices - 2
-#else:
- #   total += prices
-#print(f"Total Bill:" , total , "(Even price item discount applied)")
+for item, price in combined_list:
 
-#if prices % 5 == 0:
- #   total /= prices - 5
-#else:
- #   total /= prices
-#print(f"Total Bill:" , total , "(Special Offer Item)")
+    discounted_price = price
 
-#expensive_items = sum(prices for items, prices in combined_list)
-#if expensive_items in combined_list:
- #   if prices in expensive_items > 50:
-  #      print(f"Number of expensive items:", expensive_items)
- 
+    if price % 2 == 0:
+        discounted_price -= 2
+        print(f"Even price discount applied on {item}")
 
+    if price % 5 == 0:
+        discounted_price -= 1
+        print(f"Special Offer Item applied on {item}")
 
+    print(f"{item} original price: {price}")
+    print(f"{item} final price: {discounted_price}")
 
+    total += discounted_price
+
+print("Final Total:", total)
+
+expensive_items = 0
+
+for item, price in combined_list:
+    if price >= 50:
+        expensive_items += 1
+        print(f"{item} is Expensive")
+print(f"Number of expensive items: {expensive_items}")
+
+highest_price = 0
+expensive_items = " "
+
+for item, price in combined_list:
+    if price > highest_price:
+        highest_price = price
+        expensive_items = item        
+print(f"Most Expensive Item : ", expensive_items)
+print(f"Price:", highest_price)
+    
+discount = 0
+if total > 200:
+    discount = 10
+    total -= discount
+    print("Festive Discount Applied :", discount)
+
+print("Final Total :", total)
+        
+        
+    
+    
+
+    
